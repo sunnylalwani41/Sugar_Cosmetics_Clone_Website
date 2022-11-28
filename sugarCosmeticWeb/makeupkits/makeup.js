a5010a11202208a9d1e79e005d88055b357071a0
sunnylalwani41 <sunnylalwani41@gmail.com>
2022-11-28 22:37:08 +0530

    Project

---
+++ sugarCosmeticWeb/makeupkits/makeup.js
@@ -0,0 +1,271 @@
+
+
+let trendingdata =[
+  
+  {
+      img: "https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-cheat-sheet-clarifying-mask-12775754203219.jpg?v=1619113702",
+      name: "CHEAT SHEET CLARIFYING MASK",
+      price: 99,
+      idtype: "combo",
+      rating: "4.9"
+  },
+  {
+      img: "https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-citrus-got-real-daily-moisturizer-28038347292755.jpg?v=1619155748",
+      name: "CITRUS GOT REAL DAILY MOISTURIZER",
+      price: 499,
+      idtype: "foundation",
+      rating:" 4.9"
+  },
+ 
+  {
+      img: "https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-cheat-sheet-anti-aging-mask-12775753744467.jpg?v=1619113715",
+      name: "CHEAT SHEET ANTI-AGING MASK",
+      price: 99,
+      idtype: "foundation",
+      rating:" 4.8"
+  },
+  {
+      img: "https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-citrus-got-real-spf30-sunscreen-28038344736851.jpg?v=1619155723",
+      name: "CITRUS GOT REAL SPF3O SUNSCREEN",
+      price: 399,
+      rating: "4.7",
+      idtype: "combo",
+  },
+   //
+  {
+          name: " NOTHING ELSE MATTER LONGWEAR LIPSTICK",
+          price: 499,
+          rating: "4.7",
+          idtype: "eye",
+          img: "https://cdn.shopify.com/s/files/1/0906/2558/products/1_1da9dee5-9c2e-4135-994c-9b7d2ca23e6b.jpg?v=1641915296 ",
+          idtype: "combo",
+      },
+
+   
+
+      {
+          name: "SMUDGE NOT MINIS-MINT GREEN ",
+          price:  699,
+          rating: "4.9",
+          category:"sugarmerchstation",
+          idtype: "face",
+          img: "https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-smudge-me-not-liquid-lipstick-mint-green-minis-set-14792910340179.jpg?v=1620652722 "
+      },
+
+      {
+          name: "SMUDGE ME NOT LIP DUOS ",
+          price: 599,
+          rating: "4.5",
+          category: "bestseller",
+          idtype: "face",
+          img: " https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-smudge-me-not-lip-duo-01-brazen-raisin-burgundy-13200661643347.jpg?v=1619108721"
+      },
+      //
+      {
+          name: "ACE OF FACE FOUNDATION STICK",
+          price: 999,
+          rating: "4.8",
+          category:"makeupkits",
+          idtype: "combo",
+          img: " https://cdn.shopify.com/s/files/1/0906/2558/products/1_529c20ee-c872-4029-9702-8580d7b9a66f.jpg?v=1640178644"
+      },
+
+      {
+          name: "Gloss Boss 24HR Eyeliner - 01 Back In Black (Black)",
+          price: 499,
+          rating: " :star:4.8",
+          idtype: "eye",
+          prodID:"11",
+          img: "https://cdn.shopify.com/s/files/1/0906/2558/products/sugar-cosmetics-gloss-boss-24hr-eyeliner-01-back-in-black-black-12771395436627_a805d7b4-0863-4ae0-9f15-a2292ae9aa93.jpg?v=1632210594 "
+      },
+          {
+   img:"https://cdn.shopify.com/s/files/1/0906/2558/products/BlendTrendFaceBrush-003Contour.jpg?v=1627659996",
+
+   name: "Blend Trend Face Brush - 003 Contour",
+   category:"sugarmerchstation",
+   price:399,
+   idtype: "foundation",
+   rating:"4.5(13)", 
+  },
+   {
+   img:"https://cdn.shopify.com/s/files/1/0906/2558/products/BlendTrendFaceBrush-006Highlighter.jpg?v=1627660002",
+
+   name: "Blend Trend Face Brush - 006 highlighter",
+   category:"makeupkits",
+   price:399,
+   idtype: "face",
+   rating:"4.7(23)", 
+  },
+   
+         {
+      img:"https://cdn.shopify.com/s/files/1/0906/2558/products/BlendTrendFaceBrush-007Powder.jpg?v=1627660008",
+ 
+      name: "Blend Trend Foundation Brush - 007 powder",
+      category: "sugarset",
+      price:399,
+      rating:"4.9(24)",
+      idtype: "combo",
+      },
+       {
+             name: "MATTE AS HELL CRAYON LIPSTICK ",
+             price: 399,
+             rating: "4.8",
+             category:"sugarmerchstation", 
+             prodID:"22",
+             img: " https://cdn.shopify.com/s/files/1/0906/2558/products/01_d6ffec78-c2b6-4466-8672-233fee0285a8.jpg?v=1644394232 "
+         },
+         {
+         img:"https://cdn.shopify.com/s/files/1/0906/2558/products/Blend-Trend-WBG-images-14_1.jpg?v=1627659982",
+         name:"blend trend eyeshadow brush -043 - round xl",
+         category: "bestseller",
+         price: 399,
+         rating:"4.8(6)",
+         idtype: "face",
+     },
+      {
+         img:"https://cdn.shopify.com/s/files/1/0906/2558/products/Blend-Trend-WBG-images-3-min.jpg?v=1627573657",
+         name:"blend trend eyeshadow brush -042 - round",
+         category: "bestseller", 
+         price:399,
+         rating:"4.4(5)",
+         prodID:"24",
+     },
+      {
+         img:"https://cdn.shopify.com/s/files/1/0906/2558/products/Blend-Trend-WBG-images-13.jpg?v=1627659977",
+         name:"Blend Trend Dual Eyeshadow Brush - 413 Flat + Round Xl",
+         category:"makeupkits",
+         price:399 ,
+         rating:"5(2)",
+         idtype: "combo",
+     },
+      {
+         img:"https://cdn.shopify.com/s/files/1/0906/2558/products/Blend-Trend-WBG-images-2-min.jpg?v=1627573652",
+         name:"Blend Trend Eyeshadow Brush - 041 Flat",
+          category: "valueset",
+         price: 399,
+         rating:"5(3)",
+         idtype: "face",
+     },
+   
+
+  ]
+
+let itemCount = localStorage.getItem("itemsInCart") || 0; //For how may items are in the cart
+
+localStorage.setItem("trending", JSON.stringify(trendingdata)); // store all data to localstorage
+let brushDataLS = JSON.parse(localStorage.getItem("trending")); //getting all data from localstorage
+
+displayItemCards(brushDataLS); // calling the displayItemCards function
+
+function displayItemCards(brushDataLS) {
+  document.querySelector("#left > p+p > span").innerText =
+  trendingdata.length + " " + "items";
+  document.querySelector("#container").innerHTML = "";
+  brushDataLS.forEach(function (ele) {
+    let card = document.createElement("div");
+    let cardDiv = document.createElement("div");
+    let itemImg = document.createElement("img");
+    itemImg.src = ele.img;
+
+    let itemName = document.createElement("h3");
+    itemName.innerText = ele.name;
+
+    let itemPrice = document.createElement("p");
+    itemPrice.innerText = "Rs"+ ele.price;
+
+    let ratDiv = document.createElement("div");
+
+    let itemRating = document.createElement("p");
+    itemRating.innerText = ele.rating;
+    let ratingIcon = document.createElement("img");
+    ratingIcon.src = "	https://in.sugarcosmetics.com/star_filled.png";
+
+    let btnDiv = document.createElement("div");
+    let addCart = document.createElement("p");
+    addCart.innerText = "Add to Cart";
+    addCart.class = "cart";
+
+    // adding functionality to add to cart btn
+
+    addCart.addEventListener("click", function () {
+      addToCartFunc(ele);
+      itemCount++;
+      localStorage.setItem("itemsInCart", itemCount);
+    });
+
+    let addwish = document.createElement("img");
+    addwish.src = "https://img.icons8.com/material-outlined/344/like--v1.png";
+
+    // adding functionality to wish cart btn
+    addwish.addEventListener("click", function () {
+      addTowishList(ele);
+    });
+
+    btnDiv.append(addwish, addCart);
+    ratDiv.append(ratingIcon, itemRating);
+    cardDiv.append(itemImg, itemName, itemPrice, ratDiv);
+    card.append(cardDiv, btnDiv);
+    document.querySelector("#container").append(card);
+  });
+}
+
+// creating add to cart function
+
+let cartItemsData = JSON.parse(localStorage.getItem("cartItems")) || [];
+function addToCartFunc(ele) {
+  cartItemsData.push(ele);
+  localStorage.setItem("cartItems", JSON.stringify(cartItemsData));
+  alert("item added to cart successfuly");
+}
+
+let wishItemsData = JSON.parse(localStorage.getItem("wishList")) || [];
+
+function addTowishList(ele) {
+  wishItemsData.push(ele);
+  localStorage.setItem("wishList", JSON.stringify(wishItemsData));
+  alert("item added to wishlist successfuly");
+}
+
+// filtering of the separate products using js DOM
+// *****************************************************
+
+// For check box apply
+
+document.querySelector(".btn_sec").addEventListener("click", function () {
+  let checkedValue = document.querySelector("input[type ='checkbox']:checked");
+  let filteredItem = brushDataLS.filter(function (ele) {
+    return ele.idtype === checkedValue.value;
+  });
+  displayItemCards(filteredItem);
+});
+
+// For reset all check box
+document.querySelector("#rset").addEventListener("click", function () {
+  window.location.reload();
+});
+
+// For sorting price low to high
+
+document.querySelector("#lowTohigh").addEventListener("click", function () {
+  let sortedItems = brushDataLS.sort(function (a, b) {
+    return a.price - b.price;
+  });
+
+  displayItemCards(sortedItems);
+});
+
+// For sorting price high to low
+
+document.querySelector("#highTolow").addEventListener("click", function () {
+  let sortedItems = brushDataLS.sort(function (a, b) {
+    return b.price - a.price;
+  });
+
+  displayItemCards(sortedItems);
+});
+
+// For clear all sorting items
+
+document.querySelector("#clr").addEventListener("click", function () {
+  window.location.reload();
+});
